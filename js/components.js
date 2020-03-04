680776660ed3c61c37c86b1259b19f3d4eecb5b1
Vũ Phúc Trường An <kingdragon7a8@gmail.com>
2020-03-04 23:11:42 +0700

    js update again 4/3/2020

--- js/components.js
+++ js/components.js
@@ -414,6 +414,7 @@ components.post = `
               </div>
           </div>
           <div id="upload-success" style="display:none" class="upload-success" > Bạn đã đăng bài thành công ^.^</div>
+          <div id="no-user" style="display: block;color: red;" class="upload-success"> Bạn vui lòng đăng nhập để dùng chức năng này ^.^</div>          
         </section>
       </div>
       <div class="modal-footer">
@@ -436,36 +437,7 @@ components.post = `
 
 
 `;
-components.user = `
-<div class="user-wrapper">
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-</div>
-`;
 components.loading = `<div class="flex" >
 <div class="spinner-border text-danger" role="status">
   <span class="sr-only">Loading...</span>
--- js/view.js
+++ js/view.js
@@ -463,9 +463,18 @@ view.showComponents = async function(screenName) {
       }
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "none";
+
+          postWrapper.style.display = "flex";
           navLogInBtn.style.display = "none";
           navRegisterBtn.style.display = "none";
         } else {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "block";
+          postWrapper.style.display = "none";
           navLogOutBtn.style.display = "none";
         }
       });
@@ -919,9 +928,18 @@ view.showComponents = async function(screenName) {
 
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "none";
+
+          postWrapper.style.display = "flex";
           navLogInBtn.style.display = "none";
           navRegisterBtn.style.display = "none";
         } else {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "block";
+          postWrapper.style.display = "none";
           navLogOutBtn.style.display = "none";
         }
       });
@@ -1104,9 +1122,18 @@ view.showComponents = async function(screenName) {
       }
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "none";
+
+          postWrapper.style.display = "flex";
           navLogInBtn.style.display = "none";
           navRegisterBtn.style.display = "none";
         } else {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "block";
+          postWrapper.style.display = "none";
           navLogOutBtn.style.display = "none";
         }
       });
@@ -1615,9 +1642,18 @@ view.showComponents = async function(screenName) {
       });
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "none";
+
+          postWrapper.style.display = "flex";
           navLogInBtn.style.display = "none";
           navRegisterBtn.style.display = "none";
         } else {
+          let postWrapper = document.getElementById("post-wrapper");
+          let noUser = document.getElementById("no-user");
+          noUser.style.display = "block";
+          postWrapper.style.display = "none";
           navLogOutBtn.style.display = "none";
         }
       });
